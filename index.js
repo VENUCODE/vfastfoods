1878538f80ca5b3f9d4918bd62b33b3d360dffe4
yedukondalu naik bhukya <yedukondalunaik12345@gmail.com>
2024-03-09 01:26:06 +0530

    template setup,products in database and mysql,session rendering done

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const ejs = require("ejs");
+const mysql = require("mysql");
+const session = require("express-session");
 
+const bodyParser = require("body-parser");
 const app = express();
-app.listen(5000);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: "secret" }));
+app.listen(3000);
+
 app.get("/", function (req, res) {
-  res.send("hello");
+  var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node_project",
+  });
+  conn.query("select * from products", (error, result) => {
+    res.render("pages/index", { result });
+  });
 });
